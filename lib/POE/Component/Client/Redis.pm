925cc2fe2199a1e77861e20833d6b94975ab8e4a
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-03-27 23:49:47 +0100

    Refactor the code to use Test::POE::Client::TCP isn't of rolling our own TCP client

--- lib/POE/Component/Client/Redis.pm
+++ lib/POE/Component/Client/Redis.pm
@@ -29,7 +29,7 @@ sub spawn {
 }
 
 sub connected {
-  return $_[0]->{_wheel} ? 1 : 0;
+  return $_[0]->{_redis}->server_info() ? 1 : 0;
 }
 
 sub session_id {
@@ -69,8 +69,7 @@ sub _shutdown {
   }
   delete $self->{_refs};
   delete $self->{_requests};
-  delete $self->{_factory};
-  delete $self->{_wheel};
+  $self->{_redis}->shutdown();
   $kernel->alarm_remove_all();
   $kernel->alias_remove( $_ ) for $kernel->alias_list();
   $kernel->refcount_decrement( $self->{session_id} => __PACKAGE__ ) unless $self->{alias};
@@ -96,19 +95,12 @@ sub _redis_socket_failed {
   return;
 }
 
-sub _sock_up {
+sub _redis_connected {
   my ($kernel,$self,$socket,$peeraddr,$peerport) = @_[KERNEL,OBJECT,ARG0..ARG2];
-  delete $self->{_factory};
-    $self->{_wheel} = POE::Wheel::ReadWrite->new(
-    Handle => $socket,
-    Filter => POE::Filter::Redis->new(),
-    InputEvent => '_conn_input',
-    ErrorEvent => '_conn_error',
-  );
   return;
 }
 
-sub _conn_input {
+sub _redis_input {
   my ($kernel,$self,$input) = @_[KERNEL,OBJECT,ARG0];
   my $job = shift @{ $self->{_requests } };
   my $id = $job->{id};
@@ -122,9 +114,8 @@ sub _conn_input {
   return;
 }
 
-sub _conn_error {
+sub _redis_disconnected {
   my ($kernel,$self) = @_[KERNEL,OBJECT];
-  delete $self->{_socket};
   warn "Disconnected from redis server\n";
   $kernel->yield( '_shutdown' );
   return;
@@ -136,13 +127,13 @@ sub _redis_cmd {
   return unless $event;
   my @cmd = @_[ARG1..$#_];
   return unless scalar @cmd;
-  return unless $self->{_wheel};
+  return unless $self->{_redis}->server_info();
   my $sender_id = $sender->ID;
   $kernel->refcount_increment( $sender_id, __PACKAGE__ )
     unless exists $self->{_refs}->{ $sender_id };
   $self->{_refs}->{ $sender_id }++;
   push @{ $self->{_requests} }, { id => $sender_id, job => \@cmd, event => $event };
-  $self->{_wheel}->put( \@cmd );
+  $self->{_redis}->send_to_server( [ \@cmd ] );
   return;
 }
 
